8496440cd4485394869aff5f9e80f6e8f639ab7b
Abiramiazhakesan <146450760+Abiramiazhakesan@users.noreply.github.com>
2024-10-22 15:09:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const contactDetails = {
+    name: "Abirami",
+    email: "abi.shasha@example.com",
+    phone: "+91 9345756224",
+    address: {
+        street: "123 Main St",
+        city: "Chennai",
+        state: "TN",
+        zip: "12345"
+    }
+};
+
+console.log(contactDetails);
